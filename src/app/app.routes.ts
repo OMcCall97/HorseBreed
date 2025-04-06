b494abbab1a4570fe7bafef11a622f61982178e4
Ange Gardien <angegirishyaka@gmail.com>
2025-04-06 21:39:08 +0200

    fixed and organized the codes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { AboutComponent } from './components/about/about.component';
 import { HorsesComponent } from './components/horses/horses.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    {path:'appointment', component:AppointmentComponent },
     { path: 'about', component: AboutComponent },
     { path: 'horses', component: HorsesComponent },
     { path: 'signup', component: SignupComponent },
